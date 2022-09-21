d03c38b74fda20113729f116f9a9c7607eb8defd
Suciu Marius <110302280+sumarius56@users.noreply.github.com>
2022-09-21 23:42:28 +0300

    second

--- components/VideoNext.jsx
+++ components/VideoNext.jsx
@@ -8,7 +8,7 @@ const VideoNext = () => {
         <div>
           <ReactPlayer
             className="absolute top-0 left-0 right-0 bottom-0 h-full w-full"
-            url="https://youtu.be/N8GWM-1xDKQ"
+            url="https://youtu.be/N8GWM-1xDKQ?autoplay=1"
             playing={true}
             width="100%"
             height="100%"
@@ -16,9 +16,9 @@ const VideoNext = () => {
         </div>
       </div>
     </div>
-  );    
+  );
 };
 
 export default VideoNext;
 
-// absolute top-0 left-0 right-0 bottom-0
+
